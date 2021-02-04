160eeb058791f79811d39d6fab6dac1a19c0bcba
kaipengyu <Kaipeng.Yu@icf.com>
2021-02-03 23:13:46 -0500

    dupdate

--- public/render-page.js
+++ public/render-page.js
@@ -1,12 +1,12 @@
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory(require("/Users/kyu/Documents/gatsby/brill-edit-pro/node_modules/@reach/router/index.js"), require("fs"), require("/Users/kyu/Documents/gatsby/brill-edit-pro/node_modules/lodash/merge.js"), require("path"), require("react"), require("react-dom/server"), require("react-helmet"));
+		module.exports = factory(require("/Users/kyu/Documents/gatsby/node_modules/@reach/router/index.js"), require("fs"), require("/Users/kyu/Documents/gatsby/node_modules/lodash/merge.js"), require("path"), require("react"), require("react-dom/server"), require("react-helmet"));
 	else if(typeof define === 'function' && define.amd)
-		define("lib", ["/Users/kyu/Documents/gatsby/brill-edit-pro/node_modules/@reach/router/index.js", "fs", "/Users/kyu/Documents/gatsby/brill-edit-pro/node_modules/lodash/merge.js", "path", "react", "react-dom/server", "react-helmet"], factory);
+		define("lib", ["/Users/kyu/Documents/gatsby/node_modules/@reach/router/index.js", "fs", "/Users/kyu/Documents/gatsby/node_modules/lodash/merge.js", "path", "react", "react-dom/server", "react-helmet"], factory);
 	else if(typeof exports === 'object')
-		exports["lib"] = factory(require("/Users/kyu/Documents/gatsby/brill-edit-pro/node_modules/@reach/router/index.js"), require("fs"), require("/Users/kyu/Documents/gatsby/brill-edit-pro/node_modules/lodash/merge.js"), require("path"), require("react"), require("react-dom/server"), require("react-helmet"));
+		exports["lib"] = factory(require("/Users/kyu/Documents/gatsby/node_modules/@reach/router/index.js"), require("fs"), require("/Users/kyu/Documents/gatsby/node_modules/lodash/merge.js"), require("path"), require("react"), require("react-dom/server"), require("react-helmet"));
 	else
-		root["lib"] = factory(root["/Users/kyu/Documents/gatsby/brill-edit-pro/node_modules/@reach/router/index.js"], root["fs"], root["/Users/kyu/Documents/gatsby/brill-edit-pro/node_modules/lodash/merge.js"], root["path"], root["react"], root["react-dom/server"], root["react-helmet"]);
+		root["lib"] = factory(root["/Users/kyu/Documents/gatsby/node_modules/@reach/router/index.js"], root["fs"], root["/Users/kyu/Documents/gatsby/node_modules/lodash/merge.js"], root["path"], root["react"], root["react-dom/server"], root["react-helmet"]);
 })(this, function(__WEBPACK_EXTERNAL_MODULE__reach_router__, __WEBPACK_EXTERNAL_MODULE_fs__, __WEBPACK_EXTERNAL_MODULE_lodash_merge__, __WEBPACK_EXTERNAL_MODULE_path__, __WEBPACK_EXTERNAL_MODULE_react__, __WEBPACK_EXTERNAL_MODULE_react_dom_server__, __WEBPACK_EXTERNAL_MODULE_react_helmet__) {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
@@ -122,7 +122,7 @@ var plugins = [{
       "type": "Page",
       "match": "/:uid",
       "path": "/",
-      "component": "/Users/kyu/Documents/gatsby/brill-edit-pro/src/templates/page.js"
+      "component": "/Users/kyu/Documents/gatsby/src/templates/page.js"
     }]
   }
 }, {
@@ -31979,9 +31979,9 @@ function v(){return(v=Object.assign||function(e){for(var t=1;t<arguments.length;
 /***/ }),
 
 /***/ "@reach/router":
-/*!*************************************************************************************************!*\
-  !*** external "/Users/kyu/Documents/gatsby/brill-edit-pro/node_modules/@reach/router/index.js" ***!
-  \*************************************************************************************************/
+/*!**********************************************************************************!*\
+  !*** external "/Users/kyu/Documents/gatsby/node_modules/@reach/router/index.js" ***!
+  \**********************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
@@ -32001,9 +32001,9 @@ module.exports = __WEBPACK_EXTERNAL_MODULE_fs__;
 /***/ }),
 
 /***/ "lodash/merge":
-/*!******************************************************************************************!*\
-  !*** external "/Users/kyu/Documents/gatsby/brill-edit-pro/node_modules/lodash/merge.js" ***!
-  \******************************************************************************************/
+/*!***************************************************************************!*\
+  !*** external "/Users/kyu/Documents/gatsby/node_modules/lodash/merge.js" ***!
+  \***************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
